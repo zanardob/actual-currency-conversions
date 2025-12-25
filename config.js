b32d244df7c709d53ca1bbf23a9ea4c860891971
Guilherme Zanardo <12400406+zanardob@users.noreply.github.com>
2025-12-25 19:54:20 -0300

    Start adding type hints

--- config.js
+++ config.js
@@ -1,6 +1,21 @@
-// Use ISO 4217 for currency codes
+/**
+ * @typedef {Object} ConvertAccount
+ * @property {string} id - Actual Budget account ID
+ * @property {string} fromCurrency - ISO4217 currency code to convert from
+ */
+
+/**
+ * @typedef {Object} ActualConfig
+ * @property {string} syncId - Actual Budget sync ID (found under Advanced Settings)
+ * @property {ConvertAccount[]} convertAccounts - Accounts to convert transactions from
+ * @property {string} toCurrency - ISO4217 currency code to convert to
+ */
+
+/**
+ * Configuration for Actual Budget currency conversion.
+ * @type {ActualConfig}
+ */
 export const ACTUAL_CONFIG = {
-  // Under Actual > Advanced Settings
   syncId: "5a94121f-a446-4349-a21a-53b9ce4199a0",
   convertAccounts: [
     {

94cda394161ba8a2de3b58944ca6e4138e81e60e
Guilherme Zanardo <12400406+zanardob@users.noreply.github.com>
2025-12-25 18:38:48 -0300

    Remove history config

--- config.js
+++ config.js
@@ -2,8 +2,6 @@
 const Config = {
   // Under Actual > Advanced Settings
   syncId: "5a94121f-a446-4349-a21a-53b9ce4199a0",
-  // How many days of historical rates to fetch
-  history: 90,
   convertAccounts: [
     {
       id: "<your-account-id>",
